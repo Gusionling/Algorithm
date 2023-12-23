70248ba74a0ee49f25b061f7fd35b7828c34e9d5
David <isdnhk@naver.com>
2023-12-23 17:25:34 +0900

    solve (baekjoon) : 11047 동전 1

--- 교재/Greedy/모험가 길드.py
+++ 교재/Greedy/모험가 길드.py
@@ -2,31 +2,16 @@ n = int(input())
 data=list(map(int, input().split()))
 data.sort()
 
-groups=0
-group=-1
-size=0
+result=0
+count=0
 
-for i in range(len(data)):
-    if group ==-1:
-        size = data[i]
-        group = data[i]
-        group -= 1
-        if group == 0:
-            groups += 1
-            group = -1
-            size = 0
-    elif data[i] == size:
-        group -= 1
-        if group == 0:
-            groups += 1
-            group = -1
-            size = 0
-    elif data[i] > size:
-        size = data[i]
-        group = data[i] - group -1
+for i in data:
+    count += 1
+    if count>=i:
+        result += 1
+        count = 0
 
-print(groups)
-
+print(result)
 
 
     
