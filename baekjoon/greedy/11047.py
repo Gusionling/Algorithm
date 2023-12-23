063629be7977510d7b9239477c52eabe6655182a
David <isdnhk@naver.com>
2023-12-23 18:08:28 +0900

    solve(baekjoon) : 11047 동전0

---
+++ baekjoon/greedy/11047.py
@@ -0,0 +1,11 @@
+n , k = map(int , input().split())
+coin_list = list()
+for i in range(n):
+    coin_list.append(int(input()))
+
+count = 0
+for coin in reversed(coin_list):
+    count += k //coin
+    k %= coin
+
+print(count)
