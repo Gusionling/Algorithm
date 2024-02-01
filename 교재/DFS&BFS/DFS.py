182d3c9627000e2ae4e3b21678f5d009c171be0f
David <isdnhk@naver.com>
2024-02-01 20:55:15 +0900

    fix : DFS.py 예제 수정

--- 교재/DFS&BFS/DFS.py
+++ 교재/DFS&BFS/DFS.py
@@ -1,33 +1,30 @@
 # DFS 알고리즘 구현을 위한 파이썬 코드
 
-#visited=None : 사용자가 이 인자를 제공하지 않았을 때 기본 값으로 None
-def dfs(graph, start, visited=None):
-    
-    # 사용자가 visited집합을 제공하지 않았을 때 집합을 생성하여 방문한 코드들을 추적하는데 사용
-    if visited is None:
-        visited = set()
+def dfs(graph, v, visited):
+    #현재 노드를 방문 처리
+    visited[v] = True
+    print( v, end=' ')
+    #현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 
-        #현재 노드를 방문 처리
-        visited.add(start)
 
-        #현재 노드와 연결된 다른 노드를 재귀적으로 방문
-        for next_node in graph[start]:
-            if next_node not in visited:
-                dfs(graph, next_node, visited)
-        
-        return visited
+#각 노드가 연결된 정보를 표현 (2차원 리스트)
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
 
-#간단한 그래프 예시
-graph = {
-    'A': ['B', 'C'],
-    'B': ['A', 'D', 'E'],
-    'C': ['A', 'F'],
-    'D': ['B'],
-    'E': ['B', 'F'],
-    'F': ['C', 'E']
-}
+#각 노드가 방문된 정보를 표현(1차원 리스트)
+visited = [False] * 9
 
-#DFS 실행
-visited = dfs(graph, 'A')
-print("DFS 방문 순서:", visited)
-
+#정의된 DFS 함수 호출
+dfs(graph, 1, visited)
