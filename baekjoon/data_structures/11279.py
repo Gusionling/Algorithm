391e3fcd6e316ee3159a2447ba9e5f6760c7234b
David <isdnhk@naver.com>
2024-03-13 14:43:51 +0900

    solve [baekjoon] : 11279 최대 힙

---
+++ baekjoon/data_structures/11279.py
@@ -0,0 +1,20 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+q = []
+
+for _ in range(n):
+    num = int(input())
+    if num == 0:
+        if not q:
+            print(0)
+        else:
+            print(-heapq.heappop(q))
+    else:
+        heapq.heappush(q, -num)
+
+
+    
