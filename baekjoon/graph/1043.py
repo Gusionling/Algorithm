d55ba8d741c77828ebf49ff0e06053ffe5222d53
David <isdnhk@naver.com>
2024-03-25 00:07:02 +0900

    solve [baekjoon] : 1043 거짓말 (골드 4)

---
+++ baekjoon/graph/1043.py
@@ -0,0 +1,55 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+parent = [ i for i in range(n+1)]
+
+array = list(map(int, input().split()))
+known = array[1:]
+
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(x, y):
+    rootx = find(x)
+    rooty = find(y)
+
+    if rootx != rooty:
+        if rootx in known:
+            parent[rooty] = rootx
+        elif rooty in known:
+            parent[rootx] = rooty
+        else:
+            parent[rooty] = rootx
+    #서로 같은 집합에 속해 있는 경우        
+    else:
+        return
+
+
+#union - find로 초기화 작업
+parties = []
+for i in range(m):
+    party_info = list(map(int, input().split()))
+    party_len = party_info[0]
+    party = party_info[1:]
+
+    for i in range(party_len - 1):
+        union(party[i], party[i+1])
+    
+    parties.append(party)
+
+ans = 0
+for party in parties:
+    for i in party:
+        if find(i) in known:
+            break
+    else:
+        ans += 1
+
+print(ans)
+        
+
