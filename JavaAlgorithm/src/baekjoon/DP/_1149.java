4d64370627447c9a044e1064541cd1622ac7649b
David <isdnhk@naver.com>
2024-03-29 16:11:09 +0900

    solve [baekjoon] : 1149  RGB거리 (java)

--- JavaAlgorithm/src/baekjoon/DP/_1149.java
+++ JavaAlgorithm/src/baekjoon/DP/_1149.java
@@ -1,9 +1,11 @@
+package baekjoon.DP;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class _1149 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
