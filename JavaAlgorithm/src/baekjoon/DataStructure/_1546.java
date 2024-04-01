aaf9943210a67ba5357c1b12e8c4fd917024e07b
David <isdnhk@naver.com>
2024-04-01 13:49:29 +0900

    solve [baekjoon] : 1546 평균 (bronze 1)

--- JavaAlgorithm/src/baekjoon/DataStructure/_1546.java
+++ JavaAlgorithm/src/baekjoon/DataStructure/_1546.java
@@ -1,10 +1,11 @@
+package baekjoon.DataStructure;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class _1546 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
