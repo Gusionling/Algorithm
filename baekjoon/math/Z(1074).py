f98e7d765dc3ef11775a970de5c8aa0772595ab4
Gusionling <isdnhk@naver.com>
2025-01-17 12:58:26 +0900

    solve [baekjoon] : 1074 Z (gold 5)

--- baekjoon/math/Z(1074).py
+++ baekjoon/math/Z(1074).py
@@ -1,31 +1,15 @@
-import sys
-
-imput = sys.stdin.readline
+def z_visit(n, r, c):
+    if n == 0:  # 기저 사례: 2^0 = 1 크기의 배열
+        return 0
+    half = 2 ** (n - 1)  # 현재 배열의 절반 크기
+    if r < half and c < half:  # 1번 사분면
+        return z_visit(n - 1, r, c)
+    elif r < half and c >= half:  # 2번 사분면
+        return half * half + z_visit(n - 1, r, c - half)
+    elif r >= half and c < half:  # 3번 사분면
+        return 2 * half * half + z_visit(n - 1, r - half, c)
+    else:  # 4번 사분면
+        return 3 * half * half + z_visit(n - 1, r - half, c - half)
 
 N, r, c = map(int, input().split())
-location = pow(2, N) * (r+1) + c+1
-answer = 0
-
-def divide(n, l):
-    
-    global answer
-
-    if n == 1:
-        answer += l
-        return
-    
-    size = pow(4, n)
-    hole = size // l
-
-    answer += hole * pow(4, n-1)
-
-
-    divide(n-1, size % l)
-
-divide(N,location)
-
-print(answer)
-
-    
-    
-
+print(z_visit(N, r, c))
