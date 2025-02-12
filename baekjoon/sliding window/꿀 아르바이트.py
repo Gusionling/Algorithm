4fffc7e8abc71de5c09748463de8b3230ecd9b7c
Gusionling <isdnhk@naver.com>
2025-02-12 17:25:35 +0900

    solve [baekjoon] : 12847 꿀아르바이트(silver 3)

--- baekjoon/sliding window/꿀 아르바이트.py
+++ baekjoon/sliding window/꿀 아르바이트.py
@@ -1,7 +1,17 @@
-n,m = map(int, input().split())
-money = list(map(int, input().split()))
+import sys
 
-max_val = 0
-for st in range(n-(m-1)):
-    max_val = max(max_val, sum(money[st:st+m]))
-print(max_val)
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+salary = list(map(int,input().split()))
+
+sum = 0
+for i in range(m):
+    sum += salary[i]
+max_sum = sum
+
+for i in range(m, n):
+    sum = sum + salary[i] - salary[i-m]
+    max_sum = max(max_sum, sum)
+    
+print(max_sum)
