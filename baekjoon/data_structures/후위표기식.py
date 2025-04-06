c1748afd5b0cbf979a76d5281d390c54a39e5d04
Gusionling <isdnhk@naver.com>
2025-04-06 16:02:52 +0900

    solve [baekjoon] :1918 후위표기식(gold 2)

---
+++ baekjoon/data_structures/후위표기식.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline.strip()
+
+expr = list(input())
+answer = ""
+stack = deque()
+
+for cur in expr:
+    if cur.isalpha():
+        answer += cur
+    elif cur == '(':
+        stack.append(cur)
+    elif cur == '*' or cur == '/':
+        while stack and ( stack[-1] == '*' or stack[-1] == '/'):
+            answer +=  stack.pop()
+        stack.append(cur)
+    elif cur == '+' or cur == '-':
+        while stack and  stack[-1] != '(':
+            answer += stack.pop()
+        stack.append(cur)
+    else:
+        while stack and stack[-1] != '(':
+            answer +=   stack.pop()
+        stack.pop()
+    
+while stack:
+    answer += stack.pop()
+print(answer)
+
