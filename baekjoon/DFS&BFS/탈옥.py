c5491f3a3ae8f9d9f0a428e74e325a436f583e60
Gusionling <isdnhk@naver.com>
2025-09-10 09:41:21 +0900

    solve [baekjoon] :9376 탈옥(platinum4)

--- baekjoon/DFS&BFS/탈옥.py
+++ baekjoon/DFS&BFS/탈옥.py
@@ -5,91 +5,87 @@ input = sys.stdin.readline
 INF = float('inf')
 
 T = int(input())
-dr, dc = (-1,1,0,0) , (0,0,-1,1)
+dr, dc = (-1, 1, 0, 0), (0, 0, -1, 1)
 
-
-# person 은 두 사람이기에 구별하기 위해 있는 것이고 두번째 사람이라면 구현이 조금더 추가된다.
-def bfs(visited, jail, person, prisoner, h, w):
-    # 밖으로 나간 case 저장
-    result = []
+def bfs_01(start_points, jail, h, w):
+    """0-1 BFS를 사용한 최단거리 계산"""
+    visited = [[INF] * w for _ in range(h)]
+    queue = deque()
     
-    if person == 0:
-        queue = deque([prisoner[0]])
-    else:
-        queue = deque([prisoner[1]])
-    
-    visited[prisoner[person][0]][prisoner[person][1]][person] = 0
+    # 시작점들 초기화
+    for r, c in start_points:
+        if 0 <= r < h and 0 <= c < w and jail[r][c] != '*':
+            visited[r][c] = 0
+            queue.appendleft((r, c))
     
     while queue:
         r, c = queue.popleft()
         
         for i in range(4):
-            nr , nc = r + dr[i], c + dc[i]
+            nr, nc = r + dr[i], c + dc[i]
             
-            if 0<= nr < h and 0<= nc < w: 
-                if jail[nr][nc] != '*' and visited[nr][nc][person] == INF:
-                    # 죄수 1호
-                    if person == 0:
-                        # 벽인경우
-                        if jail[nr][nc] == '#':
-                            visited[nr][nc][person] = visited[r][c][person] + 1
-                        else:
-                            visited[nr][nc][person] = visited[r][c][person]
-                        
-                        queue.append((nr,nc))
-                    
-                    
-                    # 죄수 2호
+            if 0 <= nr < h and 0 <= nc < w and jail[nr][nc] != '*':
+                cost = 1 if jail[nr][nc] == '#' else 0
+                new_dist = visited[r][c] + cost
+                
+                if new_dist < visited[nr][nc]:
+                    visited[nr][nc] = new_dist
+                    if cost == 0:
+                        queue.appendleft((nr, nc))
                     else:
-                        # 죄수 1호가 거쳐간 곳이라면 그냥 얹기
-                        if visited[nr][nc][0] != INF:
-                            visited[nr][nc][1] = visited[r][c][1]
-                            
-                        else:
-                            if jail[nr][nc] == '#':
-                                visited[nr][nc][person] = visited[r][c][person] + 1
-                            else:
-                                visited[nr][nc][person] = visited[r][c][person]
-                        
-                        queue.append((nr,nc))
-
-            # 밖으로 나간 경우
-            else:
-                result.append((r, c))
-            
-                    
-    return jail, result, visited                          
-
+                        queue.append((nr, nc))
+    
+    return visited
 
 for _ in range(T):
     h, w = map(int, input().split())
     prisoner = []
-    # visited 는 INF로 초기화 하자
-    visited = [[[INF] * 2 for _ in range(w)] for _ in range(h)]
-    
     
     # 보드 입력 받기    
     jail = []
     for i in range(h):
         row = list(input().strip())
         jail.append(row)
-
+        
         for j in range(w):
             if row[j] == '$':
-                prisoner.append((i,j))
-                
-    # BFS 시작
-    jail1, result0, visited0 = bfs(visited, jail, 0, prisoner, h, w)
-    jail2, result1, visited1 = bfs(visited0, jail1, 1, prisoner, h, w)
+                prisoner.append((i, j))
     
-    print(visited1)
-    result = set(result0 + result1)
+    # 맵 확장 (상근이를 위해)
+    extended_h, extended_w = h + 2, w + 2
+    extended_jail = [['.' for _ in range(extended_w)] for _ in range(extended_h)]
     
+    # 원래 맵을 확장된 맵 중앙에 복사
+    for i in range(h):
+        for j in range(w):
+            extended_jail[i+1][j+1] = jail[i][j]
+    
+    # 1. 죄수1에서 BFS (좌표 +1)
+    dist1 = bfs_01([(prisoner[0][0]+1, prisoner[0][1]+1)], extended_jail, extended_h, extended_w)
+    
+    # 2. 죄수2에서 BFS (좌표 +1)
+    dist2 = bfs_01([(prisoner[1][0]+1, prisoner[1][1]+1)], extended_jail, extended_h, extended_w)
+    
+    # 3. 상근이(확장된 맵의 테두리)에서 BFS
+    outside_starts = []
+    for i in range(extended_h):
+        for j in range(extended_w):
+            if (i == 0 or i == extended_h-1 or j == 0 or j == extended_w-1):
+                outside_starts.append((i, j))
+    
+    dist_outside = bfs_01(outside_starts, extended_jail, extended_h, extended_w)
+    
+    # 4. 원래 맵 영역에서만 최솟값 찾기
     min_result = INF
-    for r,c in result:
-        if 0 <= r < h and 0 <= c < w:  # 안전 체크 추가
-            sum_val = visited1[r][c][0] + visited1[r][c][1]
-            #print(visited1[r][c][0] , visited1[r][c][1])
-            min_result = min(sum_val, min_result)
-    
+    for i in range(1, extended_h-1):  # 원래 맵 영역만
+        for j in range(1, extended_w-1):
+            if extended_jail[i][j] != '*':
+                total_cost = dist1[i][j] + dist2[i][j] + dist_outside[i][j]
+            
+                # 문 위치에서 만나는 경우 중복 계산 제거
+                if extended_jail[i][j] == '#':
+                    total_cost -= 2
+                
+                min_result = min(min_result, total_cost)
+                
     print(min_result)
