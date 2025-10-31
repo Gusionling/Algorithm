37d6218f2556a594359e2fac665b5cf0e62c22a1
Gusionling <isdnhk@naver.com>
2025-10-31 10:36:39 +0900

    solve [baekjoon] :6236 용돈 관리 (silver 1)

--- baekjoon/binary search/용돈 관리.py
+++ baekjoon/binary search/용돈 관리.py
@@ -1,41 +1,34 @@
 import sys
-
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-
 days = []
 for _ in range(N):
     days.append(int(input()))
-    
-    
-right = 10000
-left = 0
+
+# 범위 설정
+left = max(days)  # K는 최소한 하루 최대 금액 이상이어야 함
+right = sum(days)  # 최악의 경우 전체 합
+
+answer = right  # 초기화!
 
 while left <= right:
-    
     mid = (left + right) // 2
-    count = 1
-    money = mid
+    
+    count = 1  # 인출 횟수
+    money = mid  # 처음 인출한 금액
     
     for day in days:
-        if day > money:
-            if day > mid:
-                count = M + 1
-                break
-            money = mid - day
-            count += 1
-        else:
-            money -= day
-    
-    if count <= M:
-        answer = mid
-        right = mid - 1
+        if money < day:  # 현재 돈이 부족하면
+            count += 1  # 재인출
+            money = mid  # 새로 K원 인출
         
-    else:
-        left = mid + 1
+        money -= day  # 오늘 사용
+    
+    if count <= M:  # M번 이하로 가능
+        answer = mid
+        right = mid - 1  # 더 작은 K 시도
+    else:  # M번 초과
+        left = mid + 1  # K를 늘려야 함
 
 print(answer)
-        
-        
-    
